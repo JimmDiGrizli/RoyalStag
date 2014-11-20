c8d90de87f477dcf97f1db5e845c4ffbaae6aded
JimmDiGrizli <getmansk-y@yandex.ru>
2014-11-20 16:37:14 +0300

    Update example

--- example/example.php
+++ example/example.php
@@ -4,31 +4,98 @@ use GetSky\ParserExpressions\Context;
 
 require_once '../vendor/autoload.php';
 
-$context = new Context('1. We first parse!!');
+class DateParser
+{
 
-
-$rule = Sequence(
-    [
-        Optional(String("1. ")),
-        Optional(String("2. ")),
-        Sequence(
+    public function rule()
+    {
+        return Sequence(
             [
-                ZeroOrMore(String("!")),
-                FirstOf(
-                    [String('My'), String('You'), String('We')]
-                ),
-                String(' ')
+                $this->year(),
+                $this->dot(),
+                $this->monthy(),
+                $this->dot(),
+                $this->day(),
             ]
-        ),
-        Sequence(
-            [
-                String('first'),
-                String(' ')
-            ]
-        ),
-        String('parse'),
-        OneOrMore(String("!"))
-    ]
-);
+        );
+    }
 
-print_r($rule->scan($context)->toArray());
+    public function year()
+    {
+        return Sequence(
+            [
+                $this->digital(),
+                $this->digital(),
+                $this->digital(),
+                $this->digital()
+            ],
+            'Year'
+        );
+    }
+
+    public function monthy()
+    {
+        return Sequence(
+            [
+                $this->digital(),
+                $this->digital()
+            ],
+            'Monthly'
+        );
+    }
+
+    public function day()
+    {
+        return Sequence(
+            [
+                $this->digital(),
+                $this->digital()
+            ],
+            'Day'
+        );
+    }
+
+    public function digital()
+    {
+        return FirstOf(
+            [
+                String(0),
+                String(1),
+                String(2),
+                String(3),
+                String(4),
+                String(5),
+                String(6),
+                String(7),
+                String(8),
+                String(9)
+            ],
+            'Digital'
+        );
+    }
+
+    public function dot()
+    {
+        return Optional(
+            FirstOf(
+                [
+                    String("."),
+                    String("-"),
+                    String("/")
+                ]
+            ),
+            'Dot'
+        );
+    }
+}
+
+
+$parser = new DateParser();
+$context = new Context('2014-12-12');
+print_r($parser->rule()->scan($context)->toArray());
+
+$context = new Context('2014.01.04');
+print_r($parser->rule()->scan($context)->toArray());
+
+$context = new Context('20140409');
+print_r($parser->rule()->scan($context)->toArray());
